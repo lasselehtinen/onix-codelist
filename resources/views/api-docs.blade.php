d3554eadba739daf3d8c7b505d68748f6831edeb
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2016-03-18 22:56:11 +0200

    Added Get Postman link

--- resources/views/api-docs.blade.php
+++ resources/views/api-docs.blade.php
@@ -6,5 +6,21 @@
 @section('content')
 <h1>API documentation</h1>
 
-<p>The REST API documentation can be found in <a href="http://docs.onixcodelists.apiary.io/#">Apiary</a>.</p>
+<p>The REST API documentation can be found in <a href="http://docs.onixcodelists.apiary.io/#">Apiary</a>. There is no need for
+authentication. Below you can also find a link to a Postman collection.
+</p>
+
+<div class="postman-run-button"
+data-postman-action="collection/import"
+data-postman-var-1="3423f7fc9107d61ea3e7"></div>
+<script type="text/javascript">
+  (function (p,o,s,t,m,a,n) {
+    !p[s] && (p[s] = function () { (p[t] || (p[t] = [])).push(arguments); });
+    !o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild((
+      (n = o.createElement("script")),
+      (n.id = s+t), (n.async = 1), (n.src = m), n
+    ));
+  }(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
+</script>
+
 @endsection
